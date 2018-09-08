51c5652b4e747b4f390e180de757d30506c62ef2
DKFN <valere.tetelin@gmail.com>
2018-09-08 17:21:54 +0200

    Working YAML parsing to play-json

--- src/main/scala/freighttrain/Main.scala
+++ src/main/scala/freighttrain/Main.scala
@@ -3,10 +3,23 @@ package freighttrain
 import freighttrain.libs.StdLogger
 
 object Main {
+  def greet =
+      """
+        |
+        |                    ___                  _     __ _  _       _     _____                  _
+        |     o O O         | __|   _ _   ___    (_)   / _` || |_    | |_  |_   _|   _ _  __ _    (_)   _ _
+        |    o        _     | _|   | '_| / -_)   | |   \__, || ' \   |  _|   | |    | '_|/ _` |   | |  | ' \     _      _     ___
+        |    S__[O] _(_)_  _|_|_  _|_|_  \___|  _|_|_  |___/ |_||_|  _\__|  _|_|_  _|_|_ \__,_|  _|_|_ |_||_|  _(_)_  _(_)_  (0.1)
+        | ){======_|'''''_| ''' _|'''''_|'''''_|'''''_|''''_|''''''_|'''''_|'''''_|'''''_|'''''_|'''''_|'''''_|'''''_|'''"'_|'''''|
+        | ./o--000"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-"`-0-0-'
+        |
+      """.stripMargin
+
   def main(args: Array[String]) {
     // val conf = new freighttrain.Conf(args)  // Note: This line also works for "object freighttrain.Main extends App"
     // println("apples are: " + conf.apples())
     StdLogger.info("Welcome in FreighTrain !")
+    StdLogger.info(greet)
     YamlReader.readConfig()
   }
 }
--- src/main/scala/freighttrain/YamlReader.scala
+++ src/main/scala/freighttrain/YamlReader.scala
@@ -3,8 +3,11 @@ package freighttrain
 import java.io.{FileNotFoundException, InputStreamReader}
 
 import freighttrain.libs.{Panic, StdLogger}
+import io.circe.{Json, ParsingFailure}
 import io.circe.yaml.parser
 import org.yaml.snakeyaml.scanner.ScannerException
+import play.api.libs.json.Json
+import cats.syntax.either._
 
 import scala.io.Source
 
@@ -15,15 +18,20 @@ object YamlReader {
   def readConfig() = {
 
     try {
-      val path = s"${System.getProperty("user.dir")}/$FREIGHT_TRAIN_CONF"
-      StdLogger.info(s"Opening path :  $path")
-      val configs = Source.fromFile(path).getLines.mkString // getClass.getClassLoader.getResourceAsStream(path)
-      StdLogger.info(s"$configs")
-      val jsons = parser.parseDocuments(configs)
+        val path = s"${System.getProperty("user.dir")}/$FREIGHT_TRAIN_CONF"
+        StdLogger.info(s"Opening path :  $path")
+        val configs = Source.fromFile(path).getLines.mkString("\n")
+        StdLogger.info(s"$configs")
+
+        val jsons = play.api.libs.json.Json.parse(parser.parse(configs)
+          .leftMap(err => Panic.handled("Parsing error when converting Yaml to js", e = Some(err.underlying)))
+          .right.getOrElse(Panic.handled("Somethign went wrong with Yaml -> Json :/")).toString)
+        StdLogger.info((jsons \\ "MainContainer").mkString)
+
     } catch {
-      case npe: FileNotFoundException => Panic.handled(
+      case fnf: FileNotFoundException => Panic.handled(
         s"Could not find the main FreightTrain configuration file. Do you have a $FREIGHT_TRAIN_CONF file in current directory ?",
-        e = Some(npe)
+        e = Some(fnf)
       )
       case pars: ScannerException => Panic.handled(s"Badly formatted $FREIGHT_TRAIN_CONF file", e = Some(pars))
       case x: Throwable => Panic.wut(s"opening $FREIGHT_TRAIN_CONF", e = Some(x))
